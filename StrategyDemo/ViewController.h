d31dae283b9d97912ad78e53f393b2e557852332
Bruce chen <cqdeyouxiang@163.com>
2015-07-31 13:49:48 +0800

    Initial Commit

---
+++ StrategyDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StrategyDemo
+//
+//  Created by Bruce on 15/7/31.
+//  Copyright (c) 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ StrategyDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StrategyDemo
+//
+//  Created by Bruce on 15/7/31.
+//  Copyright (c) 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
